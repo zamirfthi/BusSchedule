5b052ab4386ea6a0e75a1265acf053494cc8d6e6
zamirfathi <b021910100@student.utem.edu.my>
2021-06-14 03:18:00 +0800

    'assignment'

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const bodyParser = require("body-parser")
+const importData = require("./data.json")
+const app = express();
+const port = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(bodyParser.json())
+
+//GET
+app.get("/students", (req, res) => {
+  res.send (importData);
+});
+
+//POST
+app.post("/schedule", async (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(port, () => {
+    console.log("Started listening on port http://localhost:5000")
+})
